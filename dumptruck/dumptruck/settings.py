01078eea47ca7ea83d6a1cc1493f733534a2cbe3
Ubuntu <ubuntu@ip-172-31-29-20.us-west-2.compute.internal>
2020-04-24 19:38:34 +0000

    DB settings

--- dumptruck/dumptruck/settings.py
+++ dumptruck/dumptruck/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dumptruck.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dumptruck',
+	'USER': 'dumptruckdbuser',
+	'PASSWORD': 'brucewillis',
+	'HOST': '44.233.52.245',
+	'PORT': '5432'
     }
 }
 
